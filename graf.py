10432694bac8b9d5add1ee8c877388d1eb8b5c4a
Tlapička <marek@tlapicka.net>
2014-03-23 23:20:30 +0100

    Ne 23.03.2014 23:20

--- graf.py
+++ graf.py
@@ -7,7 +7,25 @@
 # Úloha:   Program čte textový soubor a vykresluje graf
 ############################################################################
 
+def prevedNaCisla(seznamRetezcu):
+    """ Funkce vezme seznam řetězců,
+          - nahradí v nich desetiné čárky desesetinými tečkami
+          - převede řetězce na float
+
+        Vrátí seznam čísel
+    """
+    vysledek=[]
+    for s in seznamRetezcu: 
+        vysledek.append( float( s.replace(',','.') ) )
+    return vysledek
+
+
 jmenosouboru='graf.txt'
+import os.path
+if not os.path.isfile(jmenosouboru):
+    jmenosouboru=raw_input("Zadej jméno souboru > ")
+
+
 hodnotyX=[]
 hodnotyY=[]
 # Načtení souboru
@@ -20,18 +38,34 @@ while True:
     # zpracování řádku
     if radek=='' or '-----' in radek or radek[0]=='#':
         continue    # horizontální čáru nezpracovávám
-    x,y = radek.split() 
+    x,y = radek.split('|') 
     hodnotyX.append(x)
     hodnotyY.append(y)
+f.close()
+
 # vyndám popisky
 popisekX= hodnotyX.pop(0)
 popisekY= hodnotyY.pop(0)
 
-nahradCarky(hodnotyX)
-nahradCarky(hodnotyY)
+x = prevedNaCisla(hodnotyX)
+y = prevedNaCisla(hodnotyY)
 
-import pylab
 
-pylab.plot(hodnotyX,hodnotyY)
-pylab.show()
+import pylab as lab
+import scipy.interpolate as interpol
 
+# vytvořím si novou osu X, ktetá bude mí 300 bodů
+xx=lab.linspace(min(x),max(x),300)
+# funkce pro výpočet nových hodnoty Y
+funkceProlozeni=interpol.UnivariateSpline(x,y,s=1,k=2)
+# výpočet nových hodnot Y
+yy = funkceProlozeni(xx)
+
+lab.plot(x,y,'r+')
+lab.plot(xx,yy,'-g')
+lab.grid(True)
+lab.xlabel(popisekX.decode('UTF8'))
+lab.ylabel(popisekY.decode('UTF8'))
+
+
+lab.show()
