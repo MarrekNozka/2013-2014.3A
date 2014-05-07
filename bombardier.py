581d386eda1e8b1c7167d7f5c6a039594a4dc7e6
Tlapička <marek@tlapicka.net>
2014-05-07 09:39:56 +0200

    Bombardier -- St 07.05.2014 09:39
    	modified:   bombardier.py

--- bombardier.py
+++ bombardier.py
@@ -12,9 +12,18 @@ import pylab as lab
 import matplotlib.animation as animation
 
 ############################################
-SIRKA = 4    # 64
-VYSKA = 3    # 23
+SIRKA = 64
+VYSKA = 43
+PSLOUPCU = SIRKA
+PRADKU = VYSKA
 ############################################
+CERNA = 10
+SEDA = 5
+BILA = 0
+############################################
+"""Vrtulník je na začátku v levém horním rohu"""
+vrtulnik = [1, 0]
+data = []
 
 
 def on_key(event):
@@ -22,14 +31,42 @@ def on_key(event):
 
 
 def step(event):
-    pass
-
+    """
+    Tato funkce se opakovaně spouští každých X ms. Podle toho jak je nastaveno
+    ve funkci animation.FuncAnimation
+    """
+    global vrtulnik
+    global data
+    (r, s) = vrtulnik
+    try:
+        data[r][s] = SEDA
+    except IndexError:
+        data[r][-1] = BILA
+        r += 1
+        s = 0
+    data[r][s-1] = BILA
+    s += 1
+    vrtulnik = [r, s]
+    print vrtulnik
+    grid.set_array(data)
 
 ############################################
+#data = [[1, 2, 3, 4],
+#        [4, 5, 6, 7],
+#        [8, 9, 10, 11]]
+#data[1][0] = 11
 
-data = [[1, 2, 3, 4],
-        [4, 5, 6, 7],
-        [8, 9, 10, 11]]
+"""Vytvořím prázdnou matici číse obsahujcí nuly"""
+for r in range(PRADKU):
+    radek = [BILA for i in range(PSLOUPCU)]
+    data.append(radek)
+
+"""Vytvořím pohoří"""
+for s in range(PSLOUPCU):
+    hranice = int(0.15*VYSKA) + random.randint(0, int(0.7*VYSKA))
+    for r in range(PRADKU):
+        if r > hranice:
+            data[r][s] = CERNA
 
 
 ############################################
@@ -42,6 +79,6 @@ grid = lab.imshow(data, interpolation='none', cmap='binary')
 lab.grid()
 
 cid = fig.canvas.mpl_connect('key_press_event', on_key)
-ani = animation.FuncAnimation(fig, step, interval=300)
+anim = animation.FuncAnimation(fig, step, interval=200)
 
 lab.show()
